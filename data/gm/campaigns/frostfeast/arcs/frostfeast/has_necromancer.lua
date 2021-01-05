1769cc817aec9843ce502ae001e147fd4e13a4f1
AndrealVox <andrealvox@gmail.com>
2021-01-05 06:10:33 -0800

    Fixes
    -Fixes & Updates

--- data/gm/campaigns/frostfeast/arcs/frostfeast/has_necromancer.lua
+++ data/gm/campaigns/frostfeast/arcs/frostfeast/has_necromancer.lua
@@ -1,7 +1,7 @@
 local Box_O_Vox_has_necromancer = class()
 
 function Box_O_Vox_has_necromancer:start(ctx, info)
-	local necromancer_job = stonehearth.job:get_job_info(ctx.player_id, "box_o_vox:jobs:necromancer")
+	local necromancer_job = stonehearth.job:get_job_info(ctx.player_id, "box_o_vox:jobs:summoner")
 	if necromancer_job and necromancer_job:get_highest_level()>0 then
 		return true
 	end
